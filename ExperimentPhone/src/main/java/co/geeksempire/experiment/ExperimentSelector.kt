8222eabdf39547e9983992659e8eeb74cd3986f3
Elias Fazel <elias.fazel@gmail.com>
2022-11-28 07:17:23 -0800

    Experiments On { ~ }

--- ExperimentPhone/src/main/java/co/geeksempire/experiment/ExperimentSelector.kt
+++ ExperimentPhone/src/main/java/co/geeksempire/experiment/ExperimentSelector.kt
@@ -2,7 +2,7 @@
  * Copyright © 2022 By Geeks Empire.
  *
  * Created by Elias Fazel
- * Last modified 11/28/22, 7:15 AM
+ * Last modified 11/28/22, 7:17 AM
  *
  * Licensed Under MIT License.
  * https://opensource.org/licenses/MIT
@@ -12,7 +12,6 @@ package co.geeksempire.experiment
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import co.geeksempire.experiment.Animations.GradientAnimations
 import co.geeksempire.experiment.databinding.ExperimentSelectorLayoutBinding
 
 class ExperimentSelector : AppCompatActivity() {
@@ -23,19 +22,6 @@ class ExperimentSelector : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         experimentSelectorLayoutBinding = ExperimentSelectorLayoutBinding.inflate(layoutInflater)
         setContentView(experimentSelectorLayoutBinding.root)
-
-//        experimentSelectorLayoutBinding.root.background = GradientDrawable(GradientDrawable.Orientation.TR_BL,
-//            intArrayOf(
-//                getColor(R.color.default_color_bright),
-//                getColor(R.color.default_color_game_bright),
-//                ColorsModification().mixColors(getColor(R.color.default_color_bright), getColor(R.color.default_color_game_bright), 0.5f),
-//                getColor(R.color.default_color_game_bright))
-//        )
-
-        val gradientAnimations = GradientAnimations(applicationContext)
-        gradientAnimations.multipleGradientAnimation(experimentSelectorLayoutBinding.root).start()
-
-//        gradientAnimations.multipleGradientAnimation(experimentSelectorLayoutBinding.root).start()
 
         experimentSelectorLayoutBinding.root.setOnClickListener {
 
